1cf1adecbbb67745c3f870f7de09cb1b25ab127b
Sigma-max-stack <minibee0214@gmail.com>
2026-01-24 12:04:51 -0500

    Create comp.py

---
+++ comp.py
@@ -0,0 +1,73 @@
+#import turtle
+#x = turtle.Turtle()
+#y = 20
+#a = 90
+#for i in range(4):
+ #   x.forward(20)
+#  x.right(90)
+
+#for i in range(6,30):
+ #   if i%4==0:
+  #      print(i)
+
+#a = input('Please enter your name: ')
+#for i in range(len(a)-1,-1,-1):
+#    print(a[i]) 
+
+#a = input('Please enter a word: ')
+#b = 'a,e,i,o,u,A,E,I,O,U'
+#if a in b:
+#   print()
+
+#a = int(input('Please enter a number: '))
+#print(a**0.5)
+
+#a = int(input('Please enter a number: '))
+#b = int(input('Please enter a number: '))
+#c = int(input('Please enter a number: '))
+#x = a+b+c
+#y = (a+b+c)/3
+#print(x)
+#print(y)
+
+#import turtle
+#x = turtle.Turtle()
+#n = 90
+#y = 60
+#a = 45
+#x.forward(n)
+#x.penup()
+#x.backward(a)
+#x.pendown()
+#x.right(n)
+#x.forward(y)
+
+#a = input("Please enter your name: ")
+#for i in range(len(a)):
+#    print(a[i])
+    
+#a = input("Please enter your name: ")
+#while len(a):
+#    print(a)
+
+#a = input("Please enter name: ")
+#for i in range(len(a)):
+#    print(a[i]*(i+1))
+
+#import turtle
+#x = turtle.Turtle()
+#y = 90
+#a = 45
+#b = 180
+#x.right(y)
+#x.forward(y)
+#x.left(y)
+#x.forward(a)
+#x.left(y)
+#x.forward(y)
+#x.right(b)
+#x.forward(y)
+#x.left(y)
+#x.forward(a)
+#x.left(y)
+#x.forward(y)
