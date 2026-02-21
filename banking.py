219ca356ba2126ce0fd6a4342796d388ac9ad68b
Sigma-max-stack <minibee0214@gmail.com>
2026-02-21 12:01:43 -0500

    Create banking.py

---
+++ banking.py
@@ -0,0 +1,31 @@
+def bank_start():
+    print('Hello welcome to Adams Bank: ')
+    user = input('Please enter your name to get started: ')
+    print('Hello ' + user + ' welcome to Americas middest bank')
+    username = input("Please enter your username: ")
+    password = input("Please enter your password: ") 
+    print('Logged in')
+    question = input("What do you want to do?\n1) View balance \n2) Deposit \n3) Withdraw \n4) Close account")
+
+
+def view_balance():
+    return 
+
+def deposit():
+
+
+
+def withdraw():
+
+
+
+def close_account():
+
+
+
+def main():
+    bank_start()
+    balance = 100
+
+
+main()
